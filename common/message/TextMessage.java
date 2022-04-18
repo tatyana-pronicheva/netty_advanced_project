555baa1f7d7f2ef8574d3c0dc4ebbe0b931a84e0
tpronicheva <tpronicheva@smartlabs.tv>
2022-04-18 11:43:21 +0300

    Initial commit

---
+++ common/message/TextMessage.java
@@ -0,0 +1,19 @@
+
+public class TextMessage extends Message {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "TextMessage{" +
+                "text='" + text + '\'' +
+                '}';
+    }
+}
